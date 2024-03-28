a032a10ff508257c6f348a19a46ae14aad4ba666
gosha22008 <gosha22008@yandex.ru>
2024-03-28 08:55:56 +0300

    add view df

--- test_streamlit.py
+++ test_streamlit.py
@@ -55,18 +55,18 @@ if upload_file:
         time.sleep(0.01)
         my_bar.progress(percent_complete + 1, text=progress_text)
 
-    #отображние датафрейма
-    st.write('Вывод вашего файла')
-    df = pd.read_csv('appstore_instagram_reviews_anonymized.csv')
-    st.dataframe(df)
+#отображние датафрейма
+st.write('Вывод вашего файла')
+df = pd.read_csv('appstore_instagram_reviews_anonymized.csv')
+st.dataframe(df)
 
-    st.set_option('deprecation.showPyplotGlobalUse', False)
-    # Создаем гистограмму
-    plt.hist(df['score'])
-    plt.xlabel('Значения')
-    plt.ylabel('Частота')
-    plt.title('гистограмма оценок')
-    st.pyplot()
+st.set_option('deprecation.showPyplotGlobalUse', False)
+# Создаем гистограмму
+plt.hist(df['score'])
+plt.xlabel('Значения')
+plt.ylabel('Частота')
+plt.title('гистограмма оценок')
+st.pyplot()
 
 st.set_option('deprecation.showPyplotGlobalUse', False) 
 data = np.random.randn(1000)
